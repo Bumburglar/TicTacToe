5a50755242666a593e599c0dd2391190800df805
Jon Tan <Jon Tan@JONTAN-PC>
2016-05-23 00:01:48 -0500

    Add project files.

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+	class Game
+	{
+		public string[] gameBoard;
+		public bool playerWon;
+        public Player player1;
+        public Player player2;
+        public bool playerTurn; // When this is true, Player 1 is playing.  When this is false, player 2 is playing.
+		public Game()
+		{
+			gameBoard = new string[9];
+			for (int x = 0;x< gameBoard.Length; x++)
+			{
+				gameBoard[x] = (x+1).ToString();
+                player1 = new Player("X");
+                player2 = new Player("O");
+                playerTurn = true;
+			}
+		}
+		public void Draw(string[] gameBoard) // Can draw a text gameboard (square) of any size given the gameboard as a parameter.
+		{
+			int side = (int) Math.Sqrt(gameBoard.Length+1);
+			//Console.WriteLine(gameBoard.Length);
+			int position = 0;
+			for (int x = 1; x < side+1; x++)
+			{
+				string output = "|";
+				for (int y = 1; y < side+1; y++)
+				{
+					if (position > gameBoard.Length) break;
+					output += gameBoard[position]  + " | ";
+					position++;
+				}
+				Console.WriteLine(output);
+			}
+
+		}
+		public void setPosition(int position,Player player)
+		{
+			gameBoard[position] = player.marker;	
+		}
+        public void WinCheck()
+        {
+            #region Horizontal Win Condition
+            if (gameBoard[0] == gameBoard[1] && gameBoard[1] == gameBoard[2])
+            {
+                playerWon = true;
+            }
+            else if (gameBoard[3] == gameBoard[4] && gameBoard[4] == gameBoard[5])
+            {
+                playerWon = true;
+            }
+            else if (gameBoard[6] == gameBoard[7] && gameBoard[7] == gameBoard[8])
+            {
+                playerWon = true;
+            }
+            #endregion
+            #region Vertical Win Condition
+            {
+                if (gameBoard[0] == gameBoard[3] && gameBoard[3] == gameBoard[6])
+                {
+                    playerWon = true;
+                }
+                else if (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7])
+                {
+                    playerWon = true;
+                }
+                else if (gameBoard[2] == gameBoard[5] && gameBoard[5] == gameBoard[8])
+                {
+                    playerWon = true;
+                }
+            }
+            #endregion
+            #region Diagonal Win Condition
+            if(gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8])
+            {
+                playerWon = true;
+            }
+            else if (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6])
+            {
+                playerWon = true;
+            }
+            #endregion
+        }
+        public void Play(int position,Player player)
+        {
+            position -= 1;
+            if (position > 8)
+            {
+                Console.WriteLine("That position doesn't exist on the board");
+            }
+            else if (gameBoard[position] != "x" && gameBoard[position] != "o")
+            {
+                gameBoard[position] = player.marker;
+            }
+            else
+            {
+                Console.WriteLine("Someone already played there!");
+            }
+            
+        }
+	}
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Game game = new Game();
+			game.Draw(game.gameBoard);
+            Console.WriteLine("Press any button to begin!");
+			Console.ReadLine();
+            do
+            {
+                int choice = Int32.Parse(Console.ReadLine());
+                if (choice.GetType() !=  typeof(int))
+                {
+                    Console.WriteLine("That's not an option, sorry!");
+                }
+                else if (game.playerTurn == false)
+                {
+                    game.Play(choice, game.player2);
+                    game.Draw(game.gameBoard);
+                    game.playerTurn = true;
+                    game.WinCheck();
+                }
+                else
+                {
+                    game.Play(choice, game.player1);
+                    game.Draw(game.gameBoard);
+                    game.playerTurn = false;
+                    game.WinCheck();
+                }
+
+            } while (game.playerWon == false);
+            if (game.playerWon && game.playerTurn == false)
+            {
+                Console.WriteLine("Player 1 is the winner");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Player 2 is the winner");
+                Console.ReadLine();
+            }
+		}
+	}
+}
